b2b6633e87e5545e6af0a17c8a61f2c32d470f06
zzxn <zzxnhackman@foxmail.com>
2023-03-17 16:35:33 +0800

    fix bug

--- util/crypt/string.go
+++ util/crypt/string.go
@@ -4,6 +4,6 @@ import "hash/adler32"
 
 func HashString(s string, mod int64) int64 {
 	var cs uint32 = adler32.Checksum([]byte(s))
-	h := int64(cs) & mod // must be positive
+	h := int64(cs) % mod // must be positive
 	return h
 }
