6a6a1b18cdf936e4aaae3eb95b487dc449d9f514
zzxn <zzxnhackman@foxmail.com>
2023-03-21 10:44:06 +0800

    modify fake script

--- script/fake.py
+++ script/fake.py
@@ -1,7 +1,7 @@
 #!/bin/python3
 
 PREFIX='http://localhost:8080/api'
-SIZE = 5000
+SIZE = 1100000
 BATCH_SIZE = 10000
 CONCURRENT=500 # set it to 1 if using sqlite3
 N_USER = SIZE
@@ -56,22 +56,31 @@ import tqdm
 
 
 
-def get(url: str, params, token=None):
-    return requests.get(f'{PREFIX}{url}', params=params, headers={"Auth": token} if token else None)
+# def get(url: str, params, token=None):
+#     return requests.get(f'{PREFIX}{url}', params=params, headers={"Auth": token} if token else None)
 
-def post(url: str, json, token=None):
-    return requests.post(f'{PREFIX}{url}', json=json, headers={"Auth": token} if token else None)
+# def post(url: str, json, token=None):
+#     return requests.post(f'{PREFIX}{url}', json=json, headers={"Auth": token} if token else None)
 
 def gpost(url: str, json, token=None):
-    return grequests.post(f'{PREFIX}{url}', json=json, headers={"Auth": token} if token else None)
+    return grequests.post(f'{PREFIX}{url}', timeout=10, json=json, headers={"Auth": token} if token else None)
 
 def assertOK(res: requests.Response):
-    assert res.status_code == 200, "{} {} Got {}".format(res.request.method, res.request.url, res.json())
+    if res.status_code != 200 or ('ecode' in res.json() and res.json()['ecode'] != "0"):
+        print("{} {} Got {}".format(res.request.method, res.request.url, res.json()))
+        return False
+    return True
 
 def registerUsers(n: int):
     users = []
 
     progress = tqdm.trange(n, desc="prepare register")
+    errN = 0
+    def onerr(r, e):
+        nonlocal errN
+        errN += 1
+        progress.set_postfix({"errN": errN}, refresh=True)
+        progress.update(1)
     for batch in split_batch(range(n), BATCH_SIZE):
         regReqs = []
         for _ in batch:
@@ -83,27 +92,38 @@ def registerUsers(n: int):
                                         "vcode":"0000", "nickname": nickname})
             regReqs.append(req)
 
-        for res in grequests.imap(regReqs, size=CONCURRENT):
-            assertOK(res)
+        for res in grequests.imap(regReqs, size=CONCURRENT, exception_handler=onerr):
+            if assertOK(res):
+                users.append(res.json())
+            else:
+                onerr(None, None)
             progress.update(1)
-            users.append(res.json())
     return users
 
 
 def makePosts(users):
     postIDs = []
     progress = tqdm.trange(N_POST)
+    errN = 0
+    def onerr(r, e):
+        nonlocal errN
+        errN += 1
+        progress.set_postfix({"errN": errN}, refresh=True)
+        progress.update(1)
     for batch in split_batch(range(N_POST), BATCH_SIZE):
         postReqs = []
         for _ in batch:
             user = rng.choice(users)
             authorID = user["user_id"]
             p = {"author_id": authorID, "title": fakeTitle(), "content": fakeContent()}
-            postReqs.append(gpost('/post/create', p, user['auth_token']))
-        for res in grequests.imap(postReqs, size=CONCURRENT):
-            assertOK(res)
+            # postReqs.append(gpost('/post/create', p, user['auth_token']))
+            postReqs.append(gpost('/post/create', p))
+        for res in grequests.imap(postReqs, size=CONCURRENT, exception_handler=onerr):
+            if assertOK(res) and "post_id" in res.json():
+                postIDs.append(res.json()["post_id"])
+            else:
+                onerr(None, None)
             progress.update(1)
-            postIDs.append(res.json()["post_id"])
     return postIDs
 
 
@@ -112,6 +132,12 @@ def replyPosts(postIDs, users):
     rng.shuffle(postIDs)
     
     progress = tqdm.tqdm(postIDs, desc="prepare reply req")
+    errN = 0
+    def onerr(r, e):
+        nonlocal errN
+        errN += 1
+        progress.set_postfix({"errN": errN}, refresh=True)
+
     for batch in split_batch(postIDs, BATCH_SIZE):
         replyReqs = []
         for postID in batch:
@@ -119,11 +145,14 @@ def replyPosts(postIDs, users):
             for _ in range(nPost):
                 user = rng.choice(users)
                 p = {"author_id": user["user_id"], "post_id": str(postID), "content": fakeContent()}
-                replyReqs.append(gpost('/post/reply', p, user['auth_token']))
+                # replyReqs.append(gpost('/post/reply', p, user['auth_token']))
+                replyReqs.append(gpost('/post/reply', p))
             progress.update(1)
     
-        for res in grequests.imap(replyReqs, size=CONCURRENT):
-            assertOK(res)
+        for res in grequests.imap(replyReqs, size=CONCURRENT, exception_handler=onerr):
+            if not assertOK(res):
+                onerr(None, None)
+
     return
 
 
