63b6c016881974eacc0bc51e6ef9737e0f897326
zzxn <zzxnhackman@foxmail.com>
2023-03-22 13:40:27 +0800

    better page cursor

--- storage/pagecursor.go
+++ storage/pagecursor.go
@@ -8,36 +8,36 @@ import (
 	"time"
 )
 
-var pageCursorTimeFormat = time.RFC3339Nano
+var pageCursorTimeFormat = "2006-01-02-15-04-05.000" // pricision: ms
 
 func decomposePageCursor(cursor string) (ID int64, t time.Time, err error) {
 	if cursor == "" {
 		return math.MaxInt64, time.Now(), nil
 	}
-	segs := strings.SplitN(strings.ReplaceAll(cursor, "P", "+"), "_", 2)
+	segs := strings.SplitN(cursor, "_", 2)
 	if len(segs) != 2 {
 		return ID, t, fmt.Errorf("Wrong page cursor format: %v", cursor)
 	}
 
-	ID, err = strconv.ParseInt(segs[0], 10, 64)
-	if err != nil {
-		return ID, t, fmt.Errorf("wrong page cursor format, expect first part a int, got %v", segs[0])
-	}
-
-	t, err = time.Parse(pageCursorTimeFormat, segs[1])
+	t, err = time.ParseInLocation(pageCursorTimeFormat, segs[0], time.UTC)
 	if err != nil {
 		return ID, t, fmt.Errorf(
-			"wrong page cursor format, expect second part time formatted, %v, got %v",
+			"wrong page cursor format, expect first part time formatted, %v, got %v",
 			pageCursorTimeFormat,
 			segs[1],
 		)
 	}
 
+	ID, err = strconv.ParseInt(segs[1], 10, 64)
+	if err != nil {
+		return ID, t, fmt.Errorf("wrong page cursor format, expect second part a int, got %v", segs[0])
+	}
+
 	return ID, t, nil
 }
 
+// compose cursor, the dict order of cursor is same as the order of tuple (t, ID)
 func composePageCursor(ID int64, t time.Time) (cursor string) {
-	cursor = fmt.Sprintf("%d_%v", ID, t.Format(pageCursorTimeFormat))
-	cursor = strings.ReplaceAll(cursor, "+", "P") // make it url safe
+	cursor = fmt.Sprintf("%v_%v", t.UTC().Format(pageCursorTimeFormat), ID) // UTC
 	return cursor
 }
